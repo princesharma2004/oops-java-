315c6a90954d3c2b709f96f47fd97e032c246a2b
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2024-08-05 23:12:43 +0530

    Update Print.java

--- L1/Print.java
+++ L1/Print.java
@@ -1,1 +1,17 @@
+/*
+    AUTHOR        - PRINCE SHARMA
+    DATE AND TIME - 5/8/24 11:15 PM
+    WORKING       - Different type of print style
+*/
 
+public class Print {
+    public static void main(String[] args) {
+        int a=5,b=10;
+        System.out.print("1\n22\n333\n4444\n");
+        System.out.println("a,b"+a+","+b);
+        System.out.println(a+",a,b,"+b);
+        System.out.println(a+""+b+"a,b");
+        System.out.println(a+b+" ab");
+        System.out.println("ab "+(a+b));
+    }
+}
