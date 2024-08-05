b12affd80b39913f3c8b7a1989e480192b113789
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2024-08-05 17:14:53 +0530

    Create Print_area_of_circle.java

---
+++ L1-home_work/Print_area_of_circle.java
@@ -0,0 +1,11 @@
+#define PI 22/7
+
+class Print_area_of_circle
+  {
+    public static void main(String[] arg)
+    {
+      int radius=5,area;
+      area=PI*PI*radius;
+      System.out.println("area of circle is - " + area + " of a radius of "+ radius);
+    }
+  }
