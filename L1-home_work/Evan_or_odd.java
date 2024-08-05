906c0ec50d4cc09c46033dedbfe13fdd13691165
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2024-08-05 18:31:28 +0530

    Create Evan_or_odd.java

---
+++ L1-home_work/Evan_or_odd.java
@@ -0,0 +1,8 @@
+public class Evan_or_odd {
+    public static void main(String[] arg)
+    {
+        int number=9;
+        if(number%2==1) System.out.println("number "+number+" is odd");
+        else System.out.println("number "+number+" is even");
+    }
+}
