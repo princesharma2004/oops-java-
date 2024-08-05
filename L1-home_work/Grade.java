78116f26c0edcf758a7bc68ba23dc24e3f3b5b63
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2024-08-05 22:54:09 +0530

    Create Grade.java

---
+++ L1-home_work/Grade.java
@@ -0,0 +1,10 @@
+public class Grade {
+    public static void main(String[] args) 
+    {
+        String grades[]={"O", "A+", "A", "B+", "B", "C", "P"},grade;
+        int percentage=81;
+        if(percentage>39) grade=grades[9-(percentage-1)/10];
+        else grade="F";
+        System.out.println("your percentage - "+percentage+" and your grde is - "+grade);
+    }
+}
