c94cab47fed686e2f84af696cc33ba583ebba551
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2024-08-06 21:21:59 +0530

    Update Print_area_of_circle.java

--- L1/home_work/Print_area_of_circle.java
+++ L1/home_work/Print_area_of_circle.java
@@ -9,8 +9,7 @@ class Print_area_of_circle
     public static void main(String[] arg)
     {
       int radius=5;
-      float area;
-      area=(float) 22/7*22/7*radius;
+      float area=(float) 22/7*22/7*radius;
       System.out.println("area of circle is " + area + " of a radius "+ radius);
     }
   }
