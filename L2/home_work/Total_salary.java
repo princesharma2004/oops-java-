8eebc0be9eb305cc1fd0115673fe3b59fdbd18e2
princesharma2004 <158800188+princesharma2004@users.noreply.github.com>
2024-08-08 23:28:28 +0530

    Create Total_salary.java

---
+++ L2/home_work/Total_salary.java
@@ -0,0 +1,23 @@
+/*
+    AUTHOR        - PRINCE SHARMA
+    DATE AND TIME - 7/8/24 11:27 PM
+    WORKING       - Total salary
+*/
+
+import java.util.Scanner;
+
+public class Total_salary {
+    public static void main(String[] args) {
+        int hourly_wage, hours_worked, total_salary;
+
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter hourly wage - ");
+        hourly_wage = obj.nextInt();
+        System.out.print("Enter hours worked - ");
+        hours_worked = obj.nextInt();
+        obj.close();
+
+        total_salary = hourly_wage * hours_worked;
+        System.out.print("The total salary is - "+total_salary);
+    }
+}
